33b7b51c932a9496c6a37f5792214fbc63bac6b4
Programming-Sai <saiahprog6@gmail.com>
2025-03-30 22:08:54 +0100

    started the initial parts of the project.

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,16 @@
+export const signup = (req, res)=>{
+    const { fullName, email, password } = req.body;
+    try{
+        
+    }
+};
+
+
+export const signin = (req, res)=>{
+    res.send("Signin Route");
+};
+
+
+export const signout = (req, res)=>{
+    res.send("Signout Route");
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+
+app.listen(process.env.PORT,  ()=>{
+    console.log(`Server is Running on ${process.env.PORT}`); 
+    connectDB()
+});
